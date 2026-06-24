9f8c65cd2320cc26451952ab76584ec84bffae62
Harsharan Rakhra <overlordharsharan@gmail.com>
2026-06-24 18:57:41 -0400

    made state machine to run ball balancing

---
+++ src/main.cpp
@@ -0,0 +1,111 @@
+#include "../include/main.h"
+#include <atomic>
+#include <chrono>
+#include <iostream>
+#include <thread>
+#include <pigpio.h>
+
+constexpr int button_left_GPIO_pin = 21;
+constexpr int button_right_GPIO_pin = 19;
+constexpr int led_red_GPIO_pin = 12;
+constexpr int led_blue_GPIO_pin = 16;
+constexpr int led_green_GPIO_pin = 13;
+
+std::atomic<bool> keepRunning(true);
+
+bool button_left_released = true;
+bool button_right_released = true;
+
+std::string state = "idle";
+
+void setup_gpio(){
+    gpioSetMode(led_red_GPIO_pin, PI_OUTPUT);
+    gpioSetMode(led_blue_GPIO_pin, PI_OUTPUT);
+    gpioSetMode(led_green_GPIO_pin, PI_OUTPUT);
+    gpioSetMode(button_left_GPIO_pin, PI_INPUT);
+    gpioSetMode(button_right_GPIO_pin, PI_INPUT);
+
+    gpioSetPullUpDown(button_left_GPIO_pin, PI_PUD_UP);
+    gpioSetPullUpDown(button_right_GPIO_pin, PI_PUD_UP);
+    
+    gpioWrite(led_red_GPIO_pin, PI_LOW);
+    gpioWrite(led_blue_GPIO_pin, PI_LOW);
+    gpioWrite(led_green_GPIO_pin, PI_LOW);
+}
+
+void set_led_colour(int red, int green, int blue){
+    gpioPWM(led_red_GPIO_pin, red);
+    gpioPWM(led_blue_GPIO_pin, blue);
+    gpioPWM(led_green_GPIO_pin, green);
+}
+
+void cleanup_gpio(){
+    gpioPWM(led_red_GPIO_pin, 0); 
+    gpioPWM(led_blue_GPIO_pin, 0); 
+    gpioPWM(led_green_GPIO_pin, 0); 
+
+    // This stops the internal pigpio PWM peripheral threads safely.
+    gpioSetMode(led_red_GPIO_pin, PI_INPUT);
+    gpioSetMode(led_blue_GPIO_pin, PI_INPUT);
+    gpioSetMode(led_green_GPIO_pin, PI_INPUT);
+}
+
+void signalHandler(int signum) {
+    keepRunning = false;
+}
+
+int main(){
+    if (gpioInitialise() < 0) {
+        std::cerr << "pigpio initialization failed." << std::endl;
+        gpioTerminate(); 
+        return 1;
+    }
+    std::cout << "pigpio initialized successfully." << std::endl;
+
+    gpioSetSignalFunc(2, signalHandler);
+    setup_gpio();
+
+    while(keepRunning){
+
+        if(state == "idle"){
+            set_led_colour(250,35,0);
+            if(gpioRead(button_left_GPIO_pin) == 0 && button_left_released) state = "ready";
+        }
+        else if(state == "ready"){
+            set_led_colour(0,0,255);
+            if(gpioRead(button_left_GPIO_pin) == 0 && gpioRead(button_right_GPIO_pin) == 0) {
+                state = "idle";
+            }
+            else if(gpioRead(button_left_GPIO_pin) == 0 && button_left_released){
+                state = "running";
+            } 
+            else if(gpioRead(button_right_GPIO_pin) == 0 && button_right_released){
+                state = "calibration";
+            }
+        }
+        else if(state == "running"){
+            set_led_colour(150,150,15);
+            if(gpioRead(button_left_GPIO_pin) == 0 && button_left_released) state = "ready";
+        }
+        else if(state == "calibration"){
+            set_led_colour(100,0,180);
+            if(gpioRead(button_right_GPIO_pin) == 0 && button_right_released) state = "ready";
+        }
+
+        if(gpioRead(button_left_GPIO_pin) == 0 && button_left_released) button_left_released = false;
+        else if(gpioRead(button_left_GPIO_pin) == 1 && !button_left_released) button_left_released = true;
+
+        if(gpioRead(button_right_GPIO_pin) == 0 && button_right_released) button_right_released = false;
+        else if(gpioRead(button_right_GPIO_pin) == 1 && !button_right_released) button_right_released = true;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    cleanup_gpio();
+    gpioTerminate();
+
+    return 0;
+}
+
+//compile
+// g++ -Wall -pthread -o pigpio_blink main.cpp -lpigpio -lyaml-cpp
